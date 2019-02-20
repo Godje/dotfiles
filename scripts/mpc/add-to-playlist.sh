58ef6ad77497b608ad74df36b07021ed09f9dc8c
Godje <comepot.4anell@gmail.com>
2019-02-20 06:59:39 -0800

    Switched to Right Control on language switch. Comments in the MPC script.

--- scripts/mpc/add-to-playlist.sh
+++ scripts/mpc/add-to-playlist.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
+#
+# Author: Daniel Mayovskiy
+#
+# Description: 
+# This script prompts you to which playlist the current track played through mpd should b
+# added. If the playlist is not in the list, and you type in the name of the new 
+# playlist it will ask you if you want to create the playlist by that name
+#
+# Dependencies:
+# dmenu, mpc, mpd, sed
+# dunst as notify-send (optional)
+#
+# Specifics:
+# Do not put the last dash in the playlists directory in $plDirectory
 
+plDirectory=~/.mpd/playlists
 track="$(mpc current -f %file%)" # full track name
 tname=$( echo $track | sed 's/.*\// /' ) # just the basename
-playlist=$( mpc lsplaylists | dmenu -l 9 -p "Add $tname to playlist:"  )
-plexists=$( mpc lsplaylists | grep -F $playlist -c )
+playlist=$( mpc lsplaylists | dmenu -l 9 -p "Add $tname to playlist:"  ) # getting the name of the playlist
+plexists=$( mpc lsplaylists | grep -F $playlist -c ) # checking if the playlist exists
 
 if [[ $playlist != "" ]] && [[ $plexists > 0 ]]; then
-	pwdpl=~/.mpd/playlists/$playlist.m3u
-	isthere=$( grep -F "$track" $pwdpl -c )
+	pwdpl=$playlistDirectory/$playlist.m3u
+	isthere=$( grep -F "$track" $pwdpl -c ) # checking to see if the file is already in the playlist.
 
 	if [[ $isthere == 0 ]]; then
 		echo $track >> $pwdpl
@@ -23,8 +38,8 @@ if [[ $playlist != "" ]] && [[ $plexists > 0 ]]; then
 else
 	confirmnew=$( echo -e "yes\nno" | dmenu -p "Create new playlist? " )
 	if [ $confirmnew == 'yes' ]; then
-		touch ~/.mpd/playlists/$playlist.m3u
-		echo $track >> ~/.mpd/playlists/$playlist.m3u
+		touch $plDirectory/$playlist.m3u
+		echo $track >> $plDirectory/$playlist.m3u
 		notify-send --urgency=low --expire-time=2350 "Playlist $playlist created" "with $tname as it's first track"
 	fi
 fi
