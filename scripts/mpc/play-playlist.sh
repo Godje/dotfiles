400e3226af8f0bc50b99a498c2f9995d45fd0ff3
Godje <comepot.4anell@gmail.com>
2019-02-20 07:04:25 -0800

    Comments on the play-playlist.sh script

--- scripts/mpc/play-playlist.sh
+++ scripts/mpc/play-playlist.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+#
+# Author:
+# Daniel Mayovskiy
+#
+# Dependencies:
+# dmenu, mpc, mpd
+#
+# Description:
+# Lists the playlists that you have available in a dmenu, and oyu just pick the one you want to play
+# It will crop the playlists that is currently playing (aka, remove everything except current song)
+# and will load the playlist you selected. 
 
 playlist=$( mpc lsplaylists | dmenu -l 9 -p "Play playlist: " )
 
 if [[ "$playlist" != "" ]]; then
 	mpc crop
-	load_output=""
 	mpc load $playlist > /dev/null
 	notify-send --urgency=low --expire-time=2000 "Playlist loaded" "Playlist: $playlist"
 fi
